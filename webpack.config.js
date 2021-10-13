bc8676fc4dec29abb724804686fcfc8d3c82b841
Radminas Usackas <usackasr@gmail.com>
2021-10-13 22:51:47 +0100

    Config Update

--- webpack.config.js
+++ webpack.config.js
@@ -1,25 +1,39 @@
 const path = require('path');
 
 module.exports = {
-    entry: './src/app.js',
-    output: {
-        filename: 'main.js',
-        path: path.resolve(__dirname, 'dist'),
-    },
-    mode: "development",
-    module: {
-        rules: [
-         { test: /\.css$/,
-           exclude: /node_modules/,
-           use: [
-             { loader: 'style-loader' },
-             { loader: 'css-loader' }
-           ]
-          },
-          {
-            test: /\.(png|svg|jpg|jpeg|gif|woff|woff2|eot|ttf|otf)$/i,
-            type: 'asset/resource',
-          }
+  entry: './src/app.js',
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  mode: "development",
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        exclude: /node_modules/,
+        use: [
+          { loader: 'style-loader' },
+          { loader: 'css-loader' }
         ]
-      }
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/i,
+        type: 'asset/resource',
+      },
+      {
+        test: /\.(png|svg|jpg|gif)$/,
+        use: [
+            {
+                loader: 'file-loader',
+                options: {
+                    name: '[name].[ext]',
+                    outputPath: 'images/',
+                    publicPath: 'images/'
+                }
+            }
+        ]
+    }
+    ]
+  }
 };
